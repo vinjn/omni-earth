b7743c6b47eeccb933018029f893ab7bfe3a32b3
vinjn <vinjn.z@gmail.com>
2023-06-30 21:32:50 -0700

    kit 105

--- run.bat
+++ run.bat
@@ -2,6 +2,7 @@
     --ext-folder %~dp0\exts ^
     --ext-folder %~dp0\app\exts ^
     --ext-folder %~dp0\app\extscache ^
+    --ext-folder %~dp0\..\cesium-omniverse\exts ^
     --enable omni.kit.profiler.window ^
     --enable omni.kit.debug.vscode ^
     --enable omni.kit.browser.sample ^
@@ -10,9 +11,11 @@
     --enable cesium.omniverse ^
     --/log/file=%~dp0\kit.log ^
     --/exts/omni.kit.registry.nucleus/registries/0/name="kit/public" ^
-    --/exts/omni.kit.registry.nucleus/registries/0/url="https://d1aiacozzchaiq.cloudfront.net/exts/kit/public/104.0/" ^
-    --/exts/omni.kit.registry.nucleus/registries/1/name="kit/internal" ^
-    --/exts/omni.kit.registry.nucleus/registries/1/url="https://dw290v42wisod.cloudfront.net/exts/kit/community" ^
+    --/exts/omni.kit.registry.nucleus/registries/0/url="https://ovextensionsprod.blob.core.windows.net/exts/kit/prod/shared/" ^
+    --/exts/omni.kit.registry.nucleus/registries/1/name="kit/sdk" ^
+    --/exts/omni.kit.registry.nucleus/registries/1/url="https://ovextensionsprod.blob.core.windows.net/exts/kit/prod/sdk/${kit_version_short}/${kit_git_hash}" ^
+    --/exts/omni.kit.registry.nucleus/registries/2/name="kit/community" ^
+    --/exts/omni.kit.registry.nucleus/registries/2/url="https://dw290v42wisod.cloudfront.net/exts/kit/community" ^
     --/app/window/showStartup=false ^
     --/app/content/emptyStageOnStart=true ^
     --/app/useFabricSceneDelegate=true ^
